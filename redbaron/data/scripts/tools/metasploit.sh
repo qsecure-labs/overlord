93b976201f553482edcf6a60ad3dbd8541c8ec59
S1kk1S <sikkis_vasilis@hotmail.com>
2019-10-04 10:49:51 +0300

    Update metasploit.sh

--- redbaron/data/scripts/tools/metasploit.sh
+++ redbaron/data/scripts/tools/metasploit.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install -y libpq-dev libpcap0.8-dev
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
-git clone https://github.com/rapid7/metasploit-framework
-cd metasploit-framework
-rvm install ruby-`(cat .ruby-version)`
-gem install bundler --no-ri --no-rdoc
-bundle install
+sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > /tmp/msfinstall
+sudo chmod 755 /tmp/msfinstall
+sudo /tmp/msfinstall
