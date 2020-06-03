5b0159689d5dcedd14b63f62c09e379cb6121d90
Vakkis <evangelosn.117@gmail.com>
2020-06-03 10:20:14 +0300

    Update: Installation commands and reqs

--- config/install.sh
+++ config/install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+#install all the packages required
 apt install python3
 apt install python3-pip
 apt install wget
 apt install unzip
+
+#install the python requirements from the txt
 pip3 install -r requirements.txt
 
-pip install ansible
+#download terraform binary
 wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
 unzip terraform_0.11.14_linux_amd64.zip
 mv terraform /opt/terraform
 rm terraform_0.11.14_linux_amd64.zip
 echo 'export PATH="$PATH:/opt"' >> ~/.profile
 
+#download godaddy plugin for terraform
 wget https://github.com/n3integration/terraform-godaddy/releases/download/v1.6.4/terraform-godaddy_linux_amd64.tgz
 tar -xvzf terraform-godaddy_linux_amd64.tgz
 rm terraform-godaddy_linux_amd64.tgz
