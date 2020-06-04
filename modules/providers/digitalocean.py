d88af450584bcdf635fcb7332a6edd22a03e15a0
Vakkis <evangelosn.117@gmail.com>
2020-06-04 17:08:05 +0300

    feat: update autossh feature to print the commands on the outputs

--- modules/providers/digitalocean.py
+++ modules/providers/digitalocean.py
@@ -12,6 +12,9 @@ module "redirector_{c["id"]}" {{
 output "redirector_{c["id"]}-ips" {{
   value = "${{module.redirector_{c["id"]}.ips}}"
 }}
+output "{c["id"]} - Run the following command on your internal DNS server" {{
+  value = "\\n\\nsocat tcp4-LISTEN:53,fork udp:localhost:53\\nsudo autossh -M 11166 -i ${{module.redirector_{c["id"]}.ips[0]}} -N -R 2222:localhost:53 root@${{module.redirector_{c["id"]}.ips[0]}}\\n"
+}}
 """
         elif c["redirector_id"] == "localhost" and c["type"]== "http":
           output = f"""
@@ -24,6 +27,9 @@ module "redirector_{c["id"]}" {{
 }}
 output "redirector_{c["id"]}-ips" {{
   value = "${{module.redirector_{c["id"]}.ips}}"
+}}
+output "{c["id"]} - Run the following command on your internal HTTP server" {{
+  value = "\\n\\nautossh -M 11166 -i ${{module.redirector_{c["id"]}.ips[0]}} -N -R 8080:localhost:80 root@${{module.redirector_{c["id"]}.ips[0]}}\\nautossh -M 11166 -i ${{module.redirector_{c["id"]}.ips[0]}} -N -R 4443:localhost:443 root@${{module.redirector_{c["id"]}.ips[0]}}\\n"
 }}
 """
         else:
