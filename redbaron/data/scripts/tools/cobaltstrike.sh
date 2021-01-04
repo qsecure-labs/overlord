62fcf1556cc1c1cd48fef0fb52a8aa6e95ebd77c
Unknown <sikkis_vasilis@hotmail.com>
2021-01-05 00:07:20 +0200

    update: cobaltstrike script

--- redbaron/data/scripts/tools/cobaltstrike.sh
+++ redbaron/data/scripts/tools/cobaltstrike.sh
@@ -2,16 +2,11 @@
 
 CSTRIKE_KEY='xxxx-xxxx-xxxx-xxxx'
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
-sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-echo "oracle-java8-installer shared/accepted-oracle-licence-v1-1 boolean true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
+sudo apt install openjdk-11-jdk -y
+sudo update-java-alternatives -s java-1.11.0-openjdk-amd64 -y
 
 token=`curl -s https://www.cobaltstrike.com/download -d "dlkey=${CSTRIKE_KEY}" | grep 'href="/downloads/' | cut -d '/' -f3`
-curl -s https://www.cobaltstrike.com/downloads/${token}/cobaltstrike-trial.tgz -o /tmp/cobaltstrike.tgz
+curl -s https://www.cobaltstrike.com/downloads/${token}/cobaltstrike-dist.tgz -o /tmp/cobaltstrike.tgz
 
 echo ${CSTRIKE_KEY} > ~/.cobaltstrike.license
 sudo cp ~/.cobaltstrike.license /root/.cobaltstrike.license
