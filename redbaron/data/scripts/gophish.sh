9813c77ffcd70954a860355c900d345960381f68
Unknown <sikkis_vasilis@hotmail.com>
2021-01-09 12:47:38 +0200

    bug(gophish): fix installation process

--- redbaron/data/scripts/gophish.sh
+++ redbaron/data/scripts/gophish.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-#install Go
-wget https://dl.google.com/go/go1.12.5.linux-amd64.tar.gz
-tar -xvf go1.12.5.linux-amd64.tar.gz
-mv go /usr/local
-rm go1.12.5.linux-amd64.tar.gz
-cd /opt && mkdir goapps && cd goapps
-export GOROOT=/usr/local/go         #its better to change the .profile file of root
-export GOPATH=/opt/goapps
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-go version
-go env
+sudo apt install unzip -y
 
 #Install Gophish
-go get github.com/gophish/gophish
-cd /opt/goapps/src/github.com/gophish/gophish
-go build
+cd /opt
+wget https://github.com/gophish/gophish/releases/download/v0.11.0/gophish-v0.11.0-linux-64bit.zip
+unzip gophish-v0.11.0-linux-64bit.zip -d gophish
+rm gophish-v0.11.0-linux-64bit.zip
+cd gophish
+chmod +x /opt/gophish/gophish
 sed -i 's/127.0.0.1/0.0.0.0/g' config.json # replace localhost to open
 
 #gophish service
 chmod +x /tmp/gophish.sh
 systemctl start gophish.service
 
-#Update .profile file
-echo "export GOROOT=/usr/local/go" >> /root/.profile
-echo "export GOPATH=/opt/goapps" >> /root/.profile
-echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> /root/.profile
-source /root/.profile
-
 #create readme file
-echo "systemctl start gophish.service (start the service)" >> /opt/goapps/src/github.com/gophish/README.txt
-echo "systemctl stop gophish.service (stop the service)" >> /opt/goapps/src/github.com/gophish/README.txt
+echo "systemctl start gophish.service (start the service)" >> /opt/gophish/OVERLORD_README.txt
+echo "systemctl stop gophish.service (stop the service)" >>/opt/gophish/OVERLORD_README.txt
 
-sleep 30s
+sleep 20s
 
-cat /var/log/gophish.err | grep 'Please login with the username admin and the password' > /opt/goapps/src/github.com/gophish/password.txt
+cat /var/log/gophish.err | grep 'Please login with the username admin and the password' > /opt/gophish/password.txt
